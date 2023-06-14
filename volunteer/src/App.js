e84b0e33f60ae39b5e3d4454935166c5b3371035
VinodHadmode <vinodhadmode@gmail.com>
2023-06-15 00:43:12 +0530

    day-2 work done

--- volunteer/src/App.js
+++ volunteer/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Homepage from "./pages/Homepage";
+import Volunteer from "./pages/Volunteer";
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      {/* <Homepage /> */}
+      <Volunteer/>
     </div>
   );
 }
